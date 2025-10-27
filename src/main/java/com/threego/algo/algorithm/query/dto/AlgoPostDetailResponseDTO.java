6515445635d761e1d791c8623807f4bcf97f2423
rosie98 <kjr2699@naver.com>
2025-10-27 14:50:43 +0900

    fix: #77 - 알고리즘 학습 게시물 조회 시 속한 로드맵 제목도 같이 가져오도록 수정

--- src/main/java/com/threego/algo/algorithm/query/dto/AlgoPostDetailResponseDTO.java
+++ src/main/java/com/threego/algo/algorithm/query/dto/AlgoPostDetailResponseDTO.java
@@ -18,10 +18,13 @@ public class AlgoPostDetailResponseDTO {
     @Schema(description = "알고리즘 학습 로드맵 ID")
     private int roadmapId;
 
+    @Schema(description = "알고리즘 학습 로드맵 제목")
+    private String roadmapTitle;
+
     @Schema(description = "알고리즘 학습 게시물 작성자 ID")
     private int memberId;
 
-    @Schema(description = "알고리즘 학습 게시물 작성자 닉넥임")
+    @Schema(description = "알고리즘 학습 게시물 작성자 닉네임")
     private String nickname;
 
     @Schema(description = "알고리즘 학습 게시물 제목")
